ebe19d1aa6a417458a84ef9f1f12cf08fc8e69bd
abdullahi abdulrahman <94324058+abdulrahman1912@users.noreply.github.com>
2023-11-24 09:25:26 +0100

    Add files via upload

---
+++ raayinkayadede/src/App.jsx
@@ -0,0 +1,19 @@
+import {React, useState} from 'react'
+import './App.css'
+import {Home, About, Contact} from "./pages";
+import {Routes, Route} from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+    <Routes>
+      <Route path= "/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes>
+  )
+}
+
+export default App
